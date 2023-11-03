e1bee372318f51ff4ebe118d73da6209f9c82079
nenikitov <nenikitov@mail.ru>
2023-11-03 19:39:10 -0400

    Init new project

--- src/format.rs
+++
@@ -1,8 +0,0 @@
-mod assets;
-mod packfile;
-mod traits;
-mod asset_table;
-mod fixed_point;
-
-pub use packfile::PackFile;
-pub use traits::{AssetConvert, AssetLoad, ConvertedFile, DataError, ExpectedData};
