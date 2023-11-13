f1a1f60da6afeb9f99f47931ba43b5d8c59b352c
UserIsntAvailable <pinedap.angel@gmail.com>
2023-11-12 21:19:34 -0500

    test(gamma_table): parse_works.

--- engine/src/asset/gamma_table.rs
+++ engine/src/asset/gamma_table.rs
@@ -1,4 +1,5 @@
-use super::{Asset, Kind};
+use super::{Asset, Extension, Kind};
+use std::mem;
 
 const ROWS_COUNT: usize = 256;
 const COLS_COUNT: usize = 101;
@@ -13,16 +14,29 @@ impl Asset for GammaTable {
         Kind::GammaTable
     }
 
-    fn parse(bytes: &[u8], _extension: &str) -> Self {
+    fn parse(bytes: &[u8], extension: Extension) -> Self {
         assert!(bytes.len() == ROWS_COUNT * COLS_COUNT);
+        assert!(extension == Extension::Dat);
 
         // SAFETY: we already check that bytes are the same length, and slices and arrays are
         // guaranteed to have the same memory layout.
-        let lookups =
-            unsafe { std::mem::transmute_copy::<_, [[u8; ROWS_COUNT]; COLS_COUNT]>(&bytes) };
+        let lookups = unsafe { mem::transmute_copy::<_, &[[u8; ROWS_COUNT]; COLS_COUNT]>(&bytes) };
 
         Self {
-            lookups: Box::new(lookups),
+            lookups: Box::new(*lookups),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_works() {
+        let bytes: Vec<_> = (0..ROWS_COUNT)
+            .flat_map(|_| vec![0u8; COLS_COUNT])
+            .collect();
+        GammaTable::parse(&bytes, Default::default());
     }
 }
