ca504990cf91b106b9dc7880f033ff5db51dc6bf
UserIsntAvailable <pinedap.angel@gmail.com>
2023-11-22 12:15:03 -0500

    feat!: `count!` macro which returns `[_; N]` or `Vec<_>`.
    Didn't really liked that we needed 2 different functions when the return type
    was the only thing that semantically was different. I guess the only disadvantage
    of this, is that you can't no longer specify the generic parameters, but that
    should not be an issue; the same `input` and `error` types are used through the
    crate's API, so there should not be any inference problems.
    
    refactor: avoid heap allocations when calling `count_const`.
    
    count_const, renamed to `__array_count`, now uses MaybeUninit to allocate a
    fixed array, instead of calling `try_into` on a vector.
    
    docs: document all the `utils::nom` items.
    
    BREAKING CHANGE: count_const is now private

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,3 +1,7 @@
+// Discussion about possible future alternatives:
+// https://github.com/rust-lang/rust/pull/101179
+#![feature(maybe_uninit_uninit_array_transpose)]
+
 pub mod asset;
 pub mod directory;
 pub mod error;
