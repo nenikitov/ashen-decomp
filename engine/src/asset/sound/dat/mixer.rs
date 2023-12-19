9085b8a505045c92566466a2210ea68ed9f61dbe
nenikitov <nenikitov@mail.ru>
2023-12-18 19:57:15 -0500

    feat: write a handy dandy `to_wav` function to convert samples to wave file

--- engine/src/asset/sound/dat/mixer.rs
+++ engine/src/asset/sound/dat/mixer.rs
@@ -34,6 +34,8 @@ impl Mixer {
 pub trait SoundEffect {
     fn pitch(self, note: u8) -> Samples;
     fn volume(self, volume: f32) -> Samples;
+
+    fn to_wave(self) -> Vec<u8>;
 }
 
 impl SoundEffect for Samples {
@@ -44,6 +46,34 @@ impl SoundEffect for Samples {
     fn volume(self, volume: f32) -> Samples {
         self.into_iter()
             .map(|s| (s as f32 * volume) as i16)
+            .collect()
+    }
+
+    fn to_wave(self) -> Vec<u8> {
+        const SAMPLE_RATE: usize = 16000;
+        const CHANNEL_COUNT: usize = 1;
+        const BITS_PER_SAMPLE: usize = 16;
+        const BYTES_PER_SAMPLE: usize = BITS_PER_SAMPLE / 8;
+
+        let size = self.len() * BYTES_PER_SAMPLE;
+
+        "RIFF"
+            .bytes()
+            .chain(u32::to_be_bytes((36 + size) as u32))
+            .chain("WAVE".bytes())
+            .chain("fmt ".bytes())
+            .chain(u32::to_le_bytes(16))
+            .chain(u16::to_le_bytes(1))
+            .chain(u16::to_le_bytes(CHANNEL_COUNT as u16))
+            .chain(u32::to_le_bytes(SAMPLE_RATE as u32))
+            .chain(u32::to_le_bytes(
+                (SAMPLE_RATE * CHANNEL_COUNT * BYTES_PER_SAMPLE) as u32,
+            ))
+            .chain(u16::to_le_bytes((CHANNEL_COUNT * BYTES_PER_SAMPLE) as u16))
+            .chain(u16::to_le_bytes(BITS_PER_SAMPLE as u16))
+            .chain("data".bytes())
+            .chain(u32::to_le_bytes(size as u32))
+            .chain(self.into_iter().flat_map(i16::to_le_bytes))
             .collect()
     }
 }
