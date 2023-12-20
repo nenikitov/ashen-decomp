4dc7391e90bf37008e36f801ce4e47c6547c3e94
nenikitov <nenikitov@mail.ru>
2023-12-19 19:21:32 -0500

    refactor: move out uncompress function

--- engine/src/asset/sound/dat/t_effect.rs
+++ engine/src/asset/sound/dat/t_effect.rs
@@ -1,7 +1,5 @@
 use std::io;
 
-use lewton::inside_ogg::OggStreamReader;
-
 use crate::{
     asset::{sound::dat::t_instrument::TSampleParsed, AssetChunk},
     utils::nom::*,
@@ -10,6 +8,7 @@ use crate::{
 use super::{
     mixer::Mixer,
     t_instrument::{TInstrument, TSample},
+    uncompress,
 };
 
 pub struct TEffect {
@@ -19,32 +18,6 @@ pub struct TEffect {
 
 // It should be separated
 impl TEffect {
-    // TODO(nenikitov): This is duplicated code from `TSong`
-
-    fn uncompress(bytes: &[u8]) -> Vec<i16> {
-        if let [b'V', b'B', u1, u2, u3, c1, c2, c3, bytes @ ..] = bytes {
-            let size_uncompressed = u32::from_le_bytes([*u1, *u2, *u3, 0]);
-            let size_compressed = u32::from_le_bytes([*c1, *c2, *c3, 0]);
-
-            let bytes = &bytes[..size_compressed as usize];
-
-            let mut data = OggStreamReader::new(io::Cursor::new(bytes))
-                .expect("Sample should be a valid OGG stream");
-            let mut samples: Vec<_> = Vec::with_capacity(size_uncompressed as usize / 2);
-            while let Ok(Some(packet)) = data.read_dec_packet_itl() {
-                samples.extend(packet);
-            }
-
-            samples
-        } else {
-            bytes
-                .array_chunks::<1>()
-                .map(|bytes| i8::from_le_bytes(*bytes) as i16)
-                .map(|sample| sample * 256)
-                .collect()
-        }
-    }
-
     pub fn mix(&self) -> Vec<i16> {
         let mut m = Mixer::new();
 
@@ -61,7 +34,7 @@ impl AssetChunk for TEffect {
         let (_, instrument) = TInstrument::parse(&input[pointers.instrument as usize..])?;
 
         let sample = {
-            let data = Self::uncompress(&input[pointers.sample_data as usize..]);
+            let data = uncompress(&input[pointers.sample_data as usize..]);
             let (_, sample) = TSample::parse(&input[pointers.sample as usize..])?;
 
             TSampleParsed::parse(
--- engine/src/asset/sound/dat/t_song.rs
+++ engine/src/asset/sound/dat/t_song.rs
@@ -5,9 +5,8 @@ use crate::{
     utils::nom::*,
 };
 use itertools::Itertools;
-use lewton::inside_ogg::OggStreamReader;
 
-use super::{mixer::SoundEffect, t_instrument::*};
+use super::{mixer::SoundEffect, t_instrument::*, uncompress};
 
 #[derive(Debug)]
 pub struct TSong {
@@ -22,43 +21,6 @@ pub struct TSong {
 }
 
 impl TSong {
-    fn uncompress(bytes: &[u8]) -> Vec<i16> {
-        if let [b'V', b'B', u1, u2, u3, c1, c2, c3, bytes @ ..] = bytes {
-            let size_uncompressed = u32::from_le_bytes([*u1, *u2, *u3, 0]);
-            let size_compressed = u32::from_le_bytes([*c1, *c2, *c3, 0]);
-
-            let bytes = &bytes[..size_compressed as usize];
-
-            // TODO(nenikitov): Use `Result`
-            let mut data = OggStreamReader::new(io::Cursor::new(bytes))
-                .expect("Sample should be a valid OGG stream");
-            let mut samples: Vec<_> = Vec::with_capacity(size_uncompressed as usize / 2);
-            // TODO(nenikitov): For whatever reason, last packet seems to be wrong. We shouldn't just ignore it.
-            while let Ok(Some(packet)) = data.read_dec_packet_itl() {
-                samples.extend(packet);
-            }
-
-            // For whatever reason, the game has the samples stored as 16-bit signed PCM,
-            // but resamples them to 8-bit PCM before playback.
-            // Which would reduce the quality of music and add unnecessary code here...
-            // It's 2023 and we can afford to play 16-bit PCM at 16000 Hz.
-
-            // TODO(nenikitov): Re-add this check
-            //assert_eq!(samples.len() * 2, size_uncompressed as usize);
-
-            samples
-        } else {
-            // Non-compressed steam is in 8-bit PCM.
-            // Because compressed data is 16-bit PCM, to keep higher quality,
-            // we need to resample non-compressed stream into 16-bit.
-            bytes
-                .array_chunks::<1>()
-                .map(|bytes| i8::from_le_bytes(*bytes) as i16)
-                .map(|sample| sample * (i16::MIN / i8::MIN as i16))
-                .collect()
-        }
-    }
-
     pub fn mix(&self) -> Vec<i16> {
         let mut m = Mixer::new();
 
@@ -145,7 +107,7 @@ impl AssetChunk for TSong {
         )?;
 
         let samples: Vec<_> = {
-            let data = Self::uncompress(&input[pointers.sample_data as usize..]);
+            let data = uncompress(&input[pointers.sample_data as usize..]);
 
             multi::count!(TSample::parse, header.sample_count as usize)(
                 &input[pointers.samples as usize..],
