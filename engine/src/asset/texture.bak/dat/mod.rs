a37a28ad1a2bc107a4d27978190c1a923b4d6c60
nenikitov <nenikitov@mail.ru>
2024-01-22 23:26:27 -0500

    feat: added texture parsing

--- engine/src/asset/texture.bak/dat/mod.rs
+++
@@ -1,2 +0,0 @@
-pub mod offset;
-pub mod texture;
--- engine/src/asset/texture/mod.rs
+++ engine/src/asset/texture/mod.rs
@@ -1,3 +1,5 @@
+use std::ops::Div;
+
 use super::{extension::*, AssetParser};
 use crate::utils::{compression::decompress, nom::*};
 use itertools::Itertools;
@@ -48,12 +50,31 @@ pub struct TextureSize {
     height: u16,
 }
 
-impl AssetParser<Wildcard> for Texture {
-    type Context<'ctx> = TextureSize;
+impl Div<u16> for TextureSize {
+    type Output = Self;
 
-    fn parser(Self::Context { width, height }: Self::Context<'_>) -> impl FnParser<Self::Output> {
-        let width = width as usize;
-        let height = height as usize;
+    fn div(self, rhs: u16) -> Self::Output {
+        &self / rhs
+    }
+}
+
+impl Div<u16> for &TextureSize {
+    type Output = TextureSize;
+
+    fn div(self, rhs: u16) -> Self::Output {
+        TextureSize {
+            width: self.width / rhs,
+            height: self.height / rhs,
+        }
+    }
+}
+
+impl AssetParser<Wildcard> for Texture {
+    type Context<'ctx> = &'ctx TextureSize;
+
+    fn parser(size: Self::Context<'_>) -> impl FnParser<Self::Output> {
+        let width = size.width as usize;
+        let height = size.height as usize;
 
         move |input| {
             let (input, colors) = multi::count!(number::le_u8, width * height)(input)?;
@@ -75,29 +96,14 @@ pub struct MippedTexture {
 }
 
 impl AssetParser<Wildcard> for MippedTexture {
-    type Context<'ctx> = TextureOffset;
+    type Context<'ctx> = TextureSize;
 
-    fn parser(offset: Self::Context<'_>) -> impl FnParser<Self::Output> {
+    fn parser(size: Self::Context<'_>) -> impl FnParser<Self::Output> {
         move |input| {
-            let input =
-                decompress(&input[offset.offset as usize..][..offset.size_compressed as usize]);
-
-            let (input, mip_1) = Texture::parser(TextureSize {
-                width: offset.width,
-                height: offset.height,
-            })(&input)?;
-            let (input, mip_2) = Texture::parser(TextureSize {
-                width: offset.width / 2,
-                height: offset.height / 2,
-            })(&input)?;
-            let (input, mip_3) = Texture::parser(TextureSize {
-                width: offset.width / 4,
-                height: offset.height / 4,
-            })(&input)?;
-            let (input, mip_4) = Texture::parser(TextureSize {
-                width: offset.width / 8,
-                height: offset.height / 8,
-            })(&input)?;
+            let (input, mip_1) = Texture::parser(&size)(&input)?;
+            let (input, mip_2) = Texture::parser(&(&size / 2))(&input)?;
+            let (input, mip_3) = Texture::parser(&(&size / 4))(&input)?;
+            let (input, mip_4) = Texture::parser(&(&size / 8))(&input)?;
 
             Ok((
                 &[],
@@ -106,5 +112,92 @@ impl AssetParser<Wildcard> for MippedTexture {
                 },
             ))
         }
+    }
+}
+
+pub struct TextureOffsetCollection {}
+
+impl AssetParser<Pack> for TextureOffsetCollection {
+    type Item = TextureOffset;
+
+    type Output = Vec<TextureOffset>;
+
+    type Context<'ctx> = ();
+
+    fn parser(context: Self::Context<'_>) -> impl FnParser<Self::Output> {
+        move |input| {
+            let (_, offsets) = multi::many0(TextureOffset::parser(()))(input)?;
+
+            Ok((&[], offsets))
+        }
+    }
+}
+
+pub struct MippedTextureCollection {}
+
+impl AssetParser<Pack> for MippedTextureCollection {
+    type Item = MippedTexture;
+
+    type Output = Vec<MippedTexture>;
+
+    type Context<'ctx> = &'ctx [TextureOffset];
+
+    fn parser(offsets: Self::Context<'_>) -> impl FnParser<Self::Output> {
+        move |input| {
+            let textures = offsets
+                .iter()
+                .map(|o| {
+                    let input = &input[o.offset as usize..][..o.size_compressed as usize];
+                    let input = decompress(input);
+
+                    MippedTexture::parser(TextureSize {
+                        width: o.width,
+                        height: o.height,
+                    })(&input)
+                    .map(|(_, d)| d)
+                })
+                .collect::<std::result::Result<Vec<_>, _>>()?;
+
+            Ok((&[], textures))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{
+        asset::color_map::{ColorMap, PaletteTexture},
+        utils::{format::*, test::*},
+    };
+    use std::{cell::LazyCell, path::PathBuf};
+
+    const COLOR_MAP_DATA: LazyCell<Vec<u8>> = deflated_file!("4F.dat");
+    const TEXTURE_INFO_DATA: LazyCell<Vec<u8>> = deflated_file!("93.dat");
+    const TEXTURE_DATA: LazyCell<Vec<u8>> = deflated_file!("95.dat");
+
+    #[test]
+    #[ignore = "uses Ashen ROM files"]
+    fn parse_rom_asset() -> eyre::Result<()> {
+        let (_, color_map) = <ColorMap as AssetParser<Pack>>::parser(())(&COLOR_MAP_DATA)?;
+        let (_, offsets) =
+            <TextureOffsetCollection as AssetParser<Pack>>::parser(())(&TEXTURE_INFO_DATA)?;
+        let (_, textures) =
+            <MippedTextureCollection as AssetParser<Pack>>::parser(&offsets)(&TEXTURE_DATA)?;
+
+        let output_dir = PathBuf::from(parsed_file_path!("textures/"));
+
+        textures.iter().enumerate().try_for_each(|(i, texture)| {
+            texture.mips.iter().enumerate().try_for_each(|(m, mip)| {
+                let file = output_dir.join(format!("{i:0>3X}-{m}.ppm"));
+
+                output_file(
+                    file,
+                    mip.colors.with_palette(&color_map.shades[15]).to_ppm(),
+                )
+            })
+        });
+
+        Ok(())
     }
 }
